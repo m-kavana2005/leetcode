f725912746289af3695b78b8e8f6df272c2f7ff1
M Kavana Reddy <168709319+m-kavana2005@users.noreply.github.com>
2025-11-05 22:18:56 +0530

    Add files via upload
    Reverse Words

---
+++ 151.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words=s.split()
+        reversed_words=words[::-1]
+        result=" ".join(reversed_words)
+        return result
