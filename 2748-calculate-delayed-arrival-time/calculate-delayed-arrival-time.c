81996cf81160ac21f93b366633ed0c95f997944c
M Kavana Reddy <168709319+m-kavana2005@users.noreply.github.com>
2026-03-12 15:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (6.45%) - LeetSync

---
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.c
@@ -0,0 +1,8 @@
+int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+    if(arrivalTime+delayedTime == 24){
+        return 0;
+    }else if (arrivalTime+delayedTime >= 24){
+        return arrivalTime + delayedTime - 24;
+    }
+    return arrivalTime + delayedTime;
+}
