b60ab1cf56c04abfb47d6461ed64c578174d6278
M Kavana Reddy <168709319+m-kavana2005@users.noreply.github.com>
2026-03-12 14:18:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.38%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isSameAfterReversals(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return True
+        elif num%10==0:
+            return False
+        else:
+            return True
