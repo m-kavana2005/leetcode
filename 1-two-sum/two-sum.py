b3f72d3e62c22dbb464d3121d249b7d92df8caab
M Kavana Reddy <168709319+m-kavana2005@users.noreply.github.com>
2026-04-13 23:02:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (58.31%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
         return []
